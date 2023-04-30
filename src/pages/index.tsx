3c3f23071fe49ab69208ce19f50ed7c488869578
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-01 08:03:08 +0900

    chore: install google fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import SketchRotation from '@/components/SketchRotation';
 
-export default function Home() {
+const Home = () => {
   return (
     <>
       <Head>
@@ -15,4 +15,6 @@ export default function Home() {
       <SketchRotation />
     </>
   );
-}
+};
+
+export default Home;
