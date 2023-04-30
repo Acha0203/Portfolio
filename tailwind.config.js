bf78797b6c41c080f399ed97353cf672c7c9858d
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-01 08:24:14 +0900

    fix: add a parenthesis to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export const theme = {
       'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
     },
     fontFamily: {
-      sans: ['var(--font--montserrat', ...fontFamily.sans],
+      sans: ['var(--font--montserrat)', ...fontFamily.sans],
     },
   },
 };
