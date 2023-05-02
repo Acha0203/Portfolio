f698a10f8b553e1fe72ab8053ec96878d2a6d99d
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-02 20:27:48 +0900

    feat: add media queries

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -6,7 +6,9 @@ import styles from '../styles/Home.module.scss';
 
 const Showcase = () => {
   return (
-    <div className={`${styles.fade_up} flex justify-center items-center w-full absolute top-80`}>
+    <div
+      className={`${styles.fade_up} ${styles.showcase_wrapper} flex flex-wrap justify-center items-center w-full absolute`}
+    >
       {sketchList.map(
         (work: {
           id: number;
