22a351c255f44e6852fe93a108d6ba7c89df0b11
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-04 04:00:14 +0900

    feat: add a hamburger menu to each sketch page

--- src/pages/sketch-book/circle-motion-01.tsx
+++ src/pages/sketch-book/circle-motion-01.tsx
@@ -1,10 +1,35 @@
+import type { MyAppState } from '@/types';
 import Head from 'next/head';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { sketchList } from '@/consts/sketchList';
+import useReload from '@/hooks/useReload';
+import useWindowSize from '@/hooks/useWindowSize';
+import { myAppActions } from '@/store/myApp';
 import CircleMotion01 from '@/components/sketch-components/CircleMotion01';
+import HamburgerBtn from '@/components/ui/HamburgerBtn';
+import HamburgerMenu from '@/components/ui/HamburgerMenu';
 import MenuBarTop from '@/components/ui/MenuBarTop';
 import styles from '../../styles/Home.module.scss';
 
 const CircleMotion01Page = () => {
+  const dispatch = useDispatch();
+  const isHamburger = useSelector((state: MyAppState) => state.myApp.isHamburger);
+  const windowWidth = useWindowSize()[0];
+
+  useReload();
+
+  // useWindowSize() を使用すると windowWidth の初期値が 0 にセットされてしまうため、メニューをクリックして画面遷移すると一瞬ハンバーガーメニューが表示されてしまう。それを防ぐために isHamburger という state を設定して、最初に画面が読み込まれた際に画面サイズを取得し、それに合わせてあらかじめ isHamburger の値をセットしておく。そして windowWidth の値が 0 の場合は isHamburger の値を参照する。
+
+  useEffect(() => {
+    if (window.innerWidth <= 768) {
+      dispatch(myAppActions.setIsHamburger(true));
+    } else {
+      dispatch(myAppActions.setIsHamburger(false));
+    }
+    dispatch(myAppActions.setIsOpen(false));
+  }, [dispatch, windowWidth]);
+
   return (
     <>
       <Head>
@@ -19,7 +44,6 @@ const CircleMotion01Page = () => {
         <div className={styles.curtain}>
           <CircleMotion01 />
         </div>
-        <MenuBarTop />
       </div>
       <div>
         <div className={`${styles.title_of_sketch} ${styles.fade_up}`}>
@@ -31,6 +55,23 @@ const CircleMotion01Page = () => {
             </a>
           </span>
         </div>
+        {windowWidth === 0 ? (
+          isHamburger ? (
+            <>
+              <HamburgerMenu />
+              <HamburgerBtn />
+            </>
+          ) : (
+            <MenuBarTop />
+          )
+        ) : windowWidth <= 768 ? (
+          <>
+            <HamburgerMenu />
+            <HamburgerBtn />
+          </>
+        ) : (
+          <MenuBarTop />
+        )}
       </div>
     </>
   );
