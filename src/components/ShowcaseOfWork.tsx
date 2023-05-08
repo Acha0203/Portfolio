596213a3fb7c2ffb8ad46d697c96415b86cb067c
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-08 18:27:20 +0900

    feat: add a mouse over animation to showcases

--- src/components/ShowcaseOfWork.tsx
+++ src/components/ShowcaseOfWork.tsx
@@ -18,7 +18,7 @@ const ShowcaseOfWork = () => {
           path: string;
         }) => {
           return (
-            <div key={work.id} className={`${styles.showcase}`}>
+            <div key={work.id} className={`${styles.showcase} relative`}>
               <Link href={work.path}>
                 <Image
                   src={`${work.thumbnailUrl}-s.png`}
@@ -26,6 +26,11 @@ const ShowcaseOfWork = () => {
                   width={250}
                   height={250}
                 />
+                <div
+                  className={`${styles.showcase_image_text} flex justify-center items-center absolute top-0 text-white text-center text-3xl`}
+                >
+                  {work.title}
+                </div>
               </Link>
             </div>
           );
