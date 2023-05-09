318a0c51230b0f5996b3859ec4f4b6499ad1c229
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-09 13:55:09 +0900

    fix: change the url for favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,29 @@ const Document = () => {
   return (
     <Html lang='en'>
       <Head>
-        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
-        <link rel='manifest' href='/site.webmanifest' />
-        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='https://acha0203.github.io/Portfolio/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='https://acha0203.github.io/Portfolio/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='https://acha0203.github.io/Portfolio/favicon-16x16.png'
+        />
+        <link rel='manifest' href='https://acha0203.github.io/Portfolio/site.webmanifest' />
+        <link
+          rel='mask-icon'
+          href='https://acha0203.github.io/Portfolio/safari-pinned-tab.svg'
+          color='#5bbad5'
+        />
         <meta name='msapplication-TileColor' content='#da532c' />
         <meta name='theme-color' content='#ffffff' />
       </Head>
