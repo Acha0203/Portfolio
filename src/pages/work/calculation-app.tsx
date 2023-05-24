966fb0fca3343a9b2b9020840701eaf282f97dfb
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-25 03:50:29 +0900

    fix: fix a bug of background

--- src/pages/work/calculation-app.tsx
+++ src/pages/work/calculation-app.tsx
@@ -49,7 +49,7 @@ const CalculationAppPage = () => {
   return (
     <>
       <MyHead title={workList[6].title} description={workList[6].description.en} />
-      <div className={`flex-col justify-center items-center relative ${styles.vh_200}`}>
+      <div className={`flex-col justify-center items-center relative ${styles.vh_280}`}>
         <SketchBackground />
         <div
           className={`flex flex-col justify-start items-center absolute ${styles.work_wrapper} ${styles.fade_up}`}
