2f9c8f7a94d3ebbd6b55110cdc1573b65b8fd94e
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-05-28 02:35:06 +0900

    fix: change the background color

--- src/components/TitleOfSketchBook.tsx
+++ src/components/TitleOfSketchBook.tsx
@@ -4,11 +4,15 @@ const TitleOfSketchBook = () => {
   return (
     <div>
       <div
-        className={`${styles.title} flex justify-center w-screen text-white absolute text-center`}
+        className={`${styles.title_of_sketch_book} flex justify-center w-screen text-white absolute text-center`}
       >
         SKETCH BOOK
       </div>
-      <div className={`${styles.description} flex justify-center w-screen absolute text-center`}>SHOWING GENERATIVE ARTS</div>
+      <div
+        className={`${styles.description_for_sketch_book_title} flex justify-center w-screen absolute text-center`}
+      >
+        SHOWING GENERATIVE ARTS
+      </div>
     </div>
   );
 };
