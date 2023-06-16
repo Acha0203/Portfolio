76c37776d3affcfbf8e63a11865b011becb9de32
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-06-17 03:08:39 +0900

    feat: modify concentric-circle-04

--- src/components/sketch-components/SketchConcentricCircle04.tsx
+++ src/components/sketch-components/SketchConcentricCircle04.tsx
@@ -10,16 +10,15 @@ const SketchConcentricCircle04 = () => {
   const arcs: Array<{ x: number; y: number; diameter: number; sWidth: number; colorCode: string }> =
     [];
   const colors = [
-    '#fbf8cc',
-    '#fde4cf',
-    '#ffcfd2',
-    '#f1c0e8',
-    '#cfbaf0',
-    '#a3c4f3',
-    '#90dbf4',
-    '#8eecf5',
-    '#98f5e1',
-    '#b9fbc0',
+    '#d8f3dc',
+    '#b7e4c7',
+    '#95d5b2',
+    '#74c69d',
+    '#52b788',
+    '#40916c',
+    '#2d6a4f',
+    '#1b4332',
+    '#081c15',
   ];
 
   const setup = (p5: p5Types, canvasParentRef: Element) => {
@@ -28,7 +27,7 @@ const SketchConcentricCircle04 = () => {
   };
 
   const draw = (p5: p5Types) => {
-    p5.background(137, 157, 255, 30);
+    p5.background(8, 28, 21, 30);
     const px = p5.random(p5.width);
     const py = p5.random(p5.height);
     const n = p5.random(p5.height / 50);
@@ -36,12 +35,12 @@ const SketchConcentricCircle04 = () => {
 
     for (let i = 0; i < n; i++) {
       const s = p5.random(1, 20);
-      arcs[i] = { x: px, y: py, diameter: d, sWidth: s, colorCode: colors[i % 10] };
+      arcs[i] = { x: px, y: py, diameter: d, sWidth: s, colorCode: colors[i % 9] };
       d += s * 2;
     }
 
     arcs.forEach((a) => {
-      p5.stroke(p5.color(a.colorCode));
+      p5.stroke(p5.color(a.colorCode + '80'));
       p5.strokeWeight(a.sWidth);
       p5.arc(a.x, a.y, a.diameter, a.diameter, p5.random(p5.TAU), p5.random(p5.TAU));
     });
