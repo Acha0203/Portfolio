6cfcfe6569a58ebfd9374772b9ad6f56e484df8e
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-08-03 17:44:33 +0900

    feractor: import the Menu component to the sketch-book page

--- src/pages/sketch-book.tsx
+++ src/pages/sketch-book.tsx
@@ -1,35 +1,14 @@
 import type { MyAppState } from '@/types';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import useWindowSize from '@/hooks/useWindowSize';
-import { myAppActions } from '@/store/myApp';
+import { useSelector } from 'react-redux';
 import MyHead from '@/components/MyHead';
 import Showcase from '@/components/Showcase';
 import SketchBackground from '@/components/sketch-components/SketchBackground';
 import TitleOfSketchBook from '@/components/TitleOfSketchBook';
-import HamburgerBtn from '@/components/ui/button/HamburgerBtn';
-import HamburgerMenu from '@/components/ui/menu/HamburgerMenu';
-import MenuBarTop from '@/components/ui/menu/MenuBarTop';
+import Menu from '@/components/ui/menu/Menu';
 import styles from '../styles/Home.module.scss';
 
 const SketchBookPage = () => {
-  const dispatch = useDispatch();
-  const { isHamburger, isInTransition } = useSelector((state: MyAppState) => state.myApp);
-  const windowWidth = useWindowSize()[0];
-
-  // useWindowSize() を使用すると windowWidth の初期値が 0 にセットされてしまうため、メニューをクリックして画面遷移すると一瞬ハンバーガーメニューが表示されてしまう。それを防ぐために isHamburger という state を設定して、最初に画面が読み込まれた際に画面サイズを取得し、それに合わせてあらかじめ isHamburger の値をセットしておく。そして windowWidth の値が 0 の場合は isHamburger の値を参照する。
-
-  useEffect(() => {
-    dispatch(myAppActions.setIsOpen(false));
-    dispatch(myAppActions.setIsInTransition(false));
-
-    if (window.innerWidth <= 1024) {
-      // window.innerWidth は useEffect() の中でしか使用できない。
-      dispatch(myAppActions.setIsHamburger(true));
-    } else {
-      dispatch(myAppActions.setIsHamburger(false));
-    }
-  }, [dispatch]);
+  const isInTransition = useSelector((state: MyAppState) => state.myApp.isInTransition);
 
   return (
     <>
@@ -48,23 +27,7 @@ const SketchBookPage = () => {
             className={`${styles.overlay} flex justify-center items-center fixed top-0 left-0 w-full h-full bg-black`}
           ></div>
         )}
-        {windowWidth === 0 ? (
-          isHamburger ? (
-            <>
-              <HamburgerMenu />
-              <HamburgerBtn />
-            </>
-          ) : (
-            <MenuBarTop />
-          )
-        ) : windowWidth <= 1024 ? (
-          <>
-            <HamburgerMenu />
-            <HamburgerBtn />
-          </>
-        ) : (
-          <MenuBarTop />
-        )}
+        <Menu />
       </div>
     </>
   );
