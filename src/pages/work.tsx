ee7ece3a7601c4b4acb965de21ec261199ef048b
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-08-03 17:51:04 +0900

    feractor: import the Menu component to the work page

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,35 +1,14 @@
 import type { MyAppState } from '@/types';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import useWindowSize from '@/hooks/useWindowSize';
-import { myAppActions } from '@/store/myApp';
+import { useSelector } from 'react-redux';
 import MyHead from '@/components/MyHead';
 import ShowcaseOfWork from '@/components/ShowcaseOfWork';
 import SketchBackground from '@/components/sketch-components/SketchBackground';
 import TitleOfWork from '@/components/TitleOfWork';
-import HamburgerBtn from '@/components/ui/button/HamburgerBtn';
-import HamburgerMenu from '@/components/ui/menu/HamburgerMenu';
-import MenuBarTop from '@/components/ui/menu/MenuBarTop';
+import Menu from '@/components/ui/menu/Menu';
 import styles from '../styles/Home.module.scss';
 
 const WorkPage = () => {
-  const dispatch = useDispatch();
-  const { isHamburger, isInTransition } = useSelector((state: MyAppState) => state.myApp);
-  const windowWidth = useWindowSize()[0];
-
-  // useWindowSize() を使用すると windowWidth の初期値が 0 にセットされてしまうため、メニューをクリックして画面遷移すると一瞬ハンバーガーメニューが表示されてしまう。それを防ぐために isHamburger という state を設定して、最初に画面が読み込まれた際に画面サイズを取得し、それに合わせてあらかじめ isHamburger の値をセットしておく。そして windowWidth の値が 0 の場合は isHamburger の値を参照する。
-
-  useEffect(() => {
-    dispatch(myAppActions.setIsOpen(false));
-    dispatch(myAppActions.setIsInTransition(false));
-
-    if (window.innerWidth <= 1024) {
-      // window.innerWidth は useEffect() の中でしか使用できない。
-      dispatch(myAppActions.setIsHamburger(true));
-    } else {
-      dispatch(myAppActions.setIsHamburger(false));
-    }
-  }, [dispatch]);
+  const isInTransition = useSelector((state: MyAppState) => state.myApp.isInTransition);
 
   return (
     <>
@@ -37,7 +16,7 @@ const WorkPage = () => {
         title='Work'
         description='This page shows various Web applications developed by Acha Ikeda, a designer and developer in Japan.'
       />
-      <div className={`flex-col justify-center items-center relative ${styles.vh_440}`}>
+      <div className={`flex-col justify-center items-center relative h-screen`}>
         <div className={styles.fade_up}>
           <SketchBackground />
           <TitleOfWork />
@@ -48,23 +27,7 @@ const WorkPage = () => {
             className={`${styles.overlay} flex justify-center items-center fixed top-0 left-0 w-full h-full bg-black`}
           ></div>
         )}
-        {windowWidth === 0 ? (
-          isHamburger ? (
-            <>
-              <HamburgerMenu />
-              <HamburgerBtn />
-            </>
-          ) : (
-            <MenuBarTop />
-          )
-        ) : windowWidth <= 1024 ? (
-          <>
-            <HamburgerMenu />
-            <HamburgerBtn />
-          </>
-        ) : (
-          <MenuBarTop />
-        )}
+        <Menu />
       </div>
     </>
   );
