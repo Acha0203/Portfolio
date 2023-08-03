cca53ad4e2815ed7bd8876402f09dfa427c0d46b
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-08-03 20:26:22 +0900

    feractor: import the Menu and Blackout component to the multiple-particle-systems-03 page

--- src/pages/sketch-book/multiple-particle-systems-03.tsx
+++ src/pages/sketch-book/multiple-particle-systems-03.tsx
@@ -1,35 +1,12 @@
-import type { MyAppState } from '@/types';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { sketchList } from '@/consts/sketchList';
-import useWindowSize from '@/hooks/useWindowSize';
-import { myAppActions } from '@/store/myApp';
+import Blackout from '@/components/Blackout';
 import MyHead from '@/components/MyHead';
 import SketchMultipleParticleSystems03 from '@/components/sketch-components/SketchMultipleParticleSystems03';
 import CodeAndBackBtn from '@/components/ui/button/CodeAndBackBtn';
-import HamburgerBtn from '@/components/ui/button/HamburgerBtn';
-import HamburgerMenu from '@/components/ui/menu/HamburgerMenu';
-import MenuBarTop from '@/components/ui/menu/MenuBarTop';
+import Menu from '@/components/ui/menu/Menu';
 import styles from '../../styles/Home.module.scss';
 
 const MultipleParticleSystems03Page = () => {
-  const dispatch = useDispatch();
-  const { isHamburger, isInTransition } = useSelector((state: MyAppState) => state.myApp);
-  const windowWidth = useWindowSize()[0];
-
-  // useWindowSize() を使用すると windowWidth の初期値が 0 にセットされてしまうため、メニューをクリックして画面遷移すると一瞬ハンバーガーメニューが表示されてしまう。それを防ぐために isHamburger という state を設定して、最初に画面が読み込まれた際に画面サイズを取得し、それに合わせてあらかじめ isHamburger の値をセットしておく。そして windowWidth の値が 0 の場合は isHamburger の値を参照する。
-
-  useEffect(() => {
-    dispatch(myAppActions.setIsOpen(false));
-    dispatch(myAppActions.setIsInTransition(false));
-
-    if (window.innerWidth <= 1024) {
-      dispatch(myAppActions.setIsHamburger(true));
-    } else {
-      dispatch(myAppActions.setIsHamburger(false));
-    }
-  }, [dispatch]);
-
   return (
     <>
       <MyHead
@@ -47,28 +24,8 @@ const MultipleParticleSystems03Page = () => {
           <div className={styles.title_of_sketch}>{`${sketchList[22].title.toUpperCase()}`}</div>
           <CodeAndBackBtn url={`${sketchList[22].codeUrl}`} prevPage='/sketch-book' />
         </div>
-        {isInTransition && (
-          <div
-            className={`${styles.overlay} flex justify-center items-center fixed top-0 left-0 w-full h-full bg-black`}
-          ></div>
-        )}
-        {windowWidth === 0 ? (
-          isHamburger ? (
-            <>
-              <HamburgerMenu />
-              <HamburgerBtn />
-            </>
-          ) : (
-            <MenuBarTop />
-          )
-        ) : windowWidth <= 1024 ? (
-          <>
-            <HamburgerMenu />
-            <HamburgerBtn />
-          </>
-        ) : (
-          <MenuBarTop />
-        )}
+        <Blackout />
+        <Menu />
       </div>
     </>
   );
