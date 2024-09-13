52a34502498b6171c2291c9a41167eb608653001
Atsuko Ikeda <ikedaatsuko@PrimaMachinaiMac2023.local>
2024-09-14 07:22:01 +0900

    Modify SketchCliffordAttractor03 and SketchBezier08

--- src/components/sketch-components/SketchBezier08.tsx
+++ src/components/sketch-components/SketchBezier08.tsx
@@ -1,29 +1,21 @@
-import type p5Types from 'p5';
-import dynamic from 'next/dynamic';
-import useWindowSize from '@/hooks/useWindowSize';
+import { NextReactP5Wrapper } from '@p5-wrapper/next';
+import { type Sketch } from '@p5-wrapper/react';
+import React from 'react';
 
-const Sketch = dynamic(import('react-p5'), {
-  loading: () => <></>,
-  ssr: false,
-});
-
-export const SketchBezier08 = () => {
-  const windowWidth = useWindowSize()[0];
-  const windowHeight = useWindowSize()[1];
-
-  const x2 = windowWidth / 3;
-  const x3 = windowWidth - x2;
+const sketch: Sketch = (p5) => {
+  const x2 = p5.windowWidth / 3;
+  const x3 = p5.windowWidth - x2;
   let y2 = 0;
-  let y3 = windowHeight;
+  let y3 = p5.windowHeight;
   let D = 1;
 
-  const setup = (p5: p5Types, canvasParentRef: Element) => {
-    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+  p5.setup = () => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight);
     p5.noFill();
     p5.stroke(255);
   };
 
-  const draw = (p5: p5Types) => {
+  p5.draw = () => {
     p5.background(0, 10);
 
     if (y2 >= p5.windowHeight || y2 <= 0) {
@@ -36,14 +28,13 @@ export const SketchBezier08 = () => {
 
     p5.circle(x2, y2, 20);
     p5.circle(x3, y3, 20);
-  };
+  }
 
-  const windowResized = (p5: p5Types) => {
-    // コンポーネントのレスポンシブ化
+  p5.windowResized = () => {
     p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
   };
-
-  return <Sketch setup={setup} draw={draw} windowResized={windowResized} />;
 };
 
-export default SketchBezier08;
+export default function SketchBezier08() {
+  return <NextReactP5Wrapper sketch={sketch} />;
+}
