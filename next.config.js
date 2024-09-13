8cfdcaeab3a01cb785ec232327bab1c6da249688
Atsuko Ikeda <ikedaatsuko@PrimaMachinaiMac2023.local>
2024-09-13 22:00:02 +0900

    modify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
@@ -16,3 +18,5 @@ module.exports = {
   },
   output: 'export',
 };
+
+module.exports = nextConfig;
